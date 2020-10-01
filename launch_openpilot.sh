2ec344da424664d45cf40930512972c16c945506
Willem Melching <willem.melching@gmail.com>
2020-10-01 16:00:52 +0200

    testing closet changes

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 
+rm -rf /data/media/0/realdata/*
+
+echo -n 1 > /data/params/d/SshEnabled
+echo -n 1 > /data/params/d/RecordFront
+echo -n 1 > /data/params/d/CommunityFeaturesToggle
+echo -n 1 > /data/params/d/IsUploadRawEnabled
+echo -n 2 > /data/params/d/HasAcceptedTerms
+echo -n "0.2.0" > /data/params/d/CompletedTrainingVersion
+
+rm -f /data/params/d/LastUpdate*
+rm -f /data/params/d/UpdateFailedCount
+
+setprop persist.neos.ssh 1
+tools/scripts/setup_ssh_keys.py commaci2
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
